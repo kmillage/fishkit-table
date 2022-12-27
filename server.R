03ddb516312430d395df348e36c3f54dd5bd94f8
Kat Millage <kmillage@ucsb.edu>
2022-12-27 12:14:43 -0800

    fixed multiple clicks on the same row

--- server.R
+++ server.R
@@ -1,109 +1,14 @@
 
-### Plotting functions and helpers --------
-
-# Colors
-s_color <- "#005387"
-c_color <- "#0096D6"
-grey_color <- "#EEEEEE"
-orange_color <- "#E88727"
-
-# Render a bar chart with a label on the left - this is the simple version used in Example Table 2
-bar_chart <- function(label, width = "100%", height = "1rem", fill = s_color, background = grey_color) {
-  bar <- div(style = list(background = fill, width = width, height = height))
-  chart <- div(style = list(flexGrow = 1, marginLeft = "0.5rem", background = background), bar)
-  div(style = list(display = "flex", alignItems = "center", color = fill), 
-      paste0(label, "%"), # text 
-      chart) # chart
-}
-
-# Render a bar chart showing the size range (as a function of the species' Lmax) that can be harvested 
-size_range_chart <- function(label, height = "0.5rem", fill = orange_color, background = grey_color, slot_limit = "N", min_size = 50, max_size = 100, Lmin = 0, Lmax = 100) {
-  
-  if(slot_limit == "N"){
-    
-    percent_out <- ((min_size)/(Lmax-Lmin))*100
-    out_bar <- div(style = list(background = background, width = paste0(round(percent_out, 2), "%"), height = height))
-    bar <- div(style = list(flexGrow = 1, marginLeft = "0.5rem", background = fill), out_bar)
-    tagList(tags$p(label),
-            bar)
-
-  }else if(slot_limit == "Y"){
-    
-    percent_low <- ((min_size)/(Lmax-Lmin))*100
-    percent_high <- ((Lmax-max_size)/(Lmax-Lmin))*100
-    percent_in <- 100-(percent_low+percent_high)
-    out_bar_low <- div(style = list(background = background, width = paste0(round(percent_low, 2), "%"), height = height, float = "left"))
-    in_bar <- div(style = list(background = fill, width = paste0(round(percent_in, 2), "%"), height = height, float = "left"))
-    out_bar_high <- div(style = list(background = background, width = paste0(round(percent_high, 2), "%"), height = height, float = "left"))
-    bar <- div(style = list(flexGrow = 1, marginLeft = "0.5rem", background = fill), out_bar_low, in_bar, out_bar_high)
-    tagList(tags$p(label),
-            bar)
-
-  }
-}
-
-# Render a donut chart with a label in the middle
-donut_theme <- theme_void()+
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_rect(color = NA_character_, fill = "transparent"),
-    plot.background = element_rect(color = NA_character_, fill = "transparent"),
-    panel.grid=element_blank(),
-    axis.ticks = element_blank(),
-    plot.title=element_blank(),
-    axis.text = element_blank(),
-    legend.position = "none"
-  )
-
-donut_chart <- function(value, fill = s_color, background = grey_color) {
-  
-  d <- tibble(category = c("value", "extra"),
-              values = c(value, 100 - value)) %>%
-    mutate(ymax = cumsum(values),
-           ymin = c(0, head(ymax, n=-1)))
-  
-  p <- ggplot(d)+
-    aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category) +
-    geom_rect() +
-    coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
-    donut_theme +
-    scale_fill_manual(values = c(background, fill))+
-    xlim(.5, 4) +
-    annotate(geom = 'text', x = 0.5, y = 0, label = paste0(value, "%"), color = fill)
-  return(p)
-}
-
-# Star making function
-rating_stars <- function(rating, max_rating = 5) {
-  star_icon <- function(empty = FALSE) {
-    tagAppendAttributes(shiny::icon("star"),
-                        style = paste("color:", if (empty) grey_color else "orange"),
-                        "aria-hidden" = "true"
-    )
-  }
-  rounded_rating <- floor(rating + 0.5)  # always round up
-  stars <- lapply(seq_len(max_rating), function(i) {
-    if (i <= rounded_rating) star_icon() else star_icon(empty = TRUE)
-  })
-  label <- sprintf("%s out of %s stars", rating, max_rating)
-  div(title = label, role = "img", stars)
-}
-
-# Little colored badges
-status_badge <- function(value, color = "#aaa", background = "#fff", border_color = "#aaa", width = "80px", height = "25px") {
-  tags$div(style = paste0("display: inline-block; padding: 0.125rem 0.75rem; font-weight: 600; font-size: 0.75rem; border: 2px solid ", border_color, "; border-radius: 15px; color:", color, "; background:", background, "; width:", width, "; height: ", height, ";"),
-           paste0(value, "%"))
-}
+### Server --------
 
 server <- function(input, output, session) {
   
   # Container storing retained user-defined options
-  # Note: Bill - You can probably streamline this process. I wanted to create a object from which all of the different example tables could draw and thus I did some formatting up front. You might be best doing any necessary formatting within the reactive call to the table since you'll only need to do it once
-  retainedData <- reactiveValues(all = NULL)
+  # Note: Bill - You can probably streamline many aspects of this process. I wanted to create a single object from which all of the different example tables could draw and thus I did some formatting up front. You might be better doing any necessary formatting within the reactive call to the table since you'll only need to do it once.
+  retainedData <- reactiveValues(all = NULL,
+                                 selected_row = NULL)
   
-  # Add a randomly drawn entry to the retained data object when the "Retain option:" button is pressed
+  ### Add a randomly drawn entry to the retained data object when the "Retain option:" button is pressed
   observeEvent(input$addRow, {
     
       # Randomly draw a row from our fake dataset to simulate user selected options
@@ -123,26 +28,51 @@ server <- function(input, output, session) {
     
   }, ignoreInit = T)
   
-  # Delete selected row from retained options
+  ### Open modal when user has clicked on a row in the table
+  observeEvent(input$table_click, {
+    
+    # Save value
+    retainedData$selected_row <- as.numeric(input$table_click) + 1 #JS row ids start from 0
+    
+    # Open modal
+    toggleModal(session, "input_modal", toggle = "open")
+    
+  })
+  
+  ### Reset selected row value when modal is closed
+  observeEvent(input$close_modal, {
+    
+    # Reset value 
+    retainedData$selected_row <- NULL
+    
+    # Close modal
+    toggleModal(session, "input_modal", toggle = "close")
+    
+  })
+  
+  ### Delete selected row from retained options and close modal
   observeEvent(input$deleteRow, {
       
       # Get selected row
-      selected_row <- as.numeric(input$table_click) + 1 #Row ids start from 0
+      selected_row <- retainedData$selected_row
       req(selected_row)
       
       # Remove row from species data 
       retainedData$all <- isolate(retainedData$all)[-selected_row,]
       
+      # Reset selected row value 
+      retainedData$selected_row <- NULL
+      
       # Close modal
-      toggleModal(session, "inputModal", toggle = "close")
+      toggleModal(session, "input_modal", toggle = "close")
 
   }, ignoreInit = T)
   
-  # Add notes to the selected row
+  ### Add notes to the selected row
   observeEvent(input$addNotes, {
     
     # Get selected row
-    selected_row <- as.numeric(input$table_click) + 1 #Row ids start from 0
+    selected_row <- retainedData$selected_row
     req(selected_row)
     
     # Add custom input notes
@@ -150,11 +80,11 @@ server <- function(input, output, session) {
     
   }, ignoreInit = T)
   
-  # Add ranking to the selected row
+  ### Add ranking to the selected row
   observeEvent(input$addRanking, {
     
     # Define selected row
-    selected_row <- as.numeric(input$table_click) + 1 #Row ids start from 0
+    selected_row <- retainedData$selected_row
     req(selected_row)
     
     # Add ranking
@@ -162,17 +92,11 @@ server <- function(input, output, session) {
     
   }, ignoreInit = T)
   
-  # Toggle modal
-  observeEvent(input$table_click, {
-
-    toggleModal(session, "inputModal", toggle = "open")
-
-  })
   
-  # UI output giving the option that was clicked on
+  ### UI output for the modal windown telling the user which option is being edited
   output$selected_row <- renderUI({
     
-    selected_row <- as.numeric(input$table_click) + 1 #Row ids start from 0
+    selected_row <- retainedData$selected_row
     req(selected_row)
     
     tags$h3(paste0("Edit Option: ", retainedData$all[selected_row, "Option"]))
@@ -210,7 +134,7 @@ server <- function(input, output, session) {
       onClick = JS("function(rowInfo, colInfo) {
     // Send the click event to Shiny
     if (window.Shiny) {
-      Shiny.setInputValue('table_click', rowInfo.id)
+      Shiny.setInputValue('table_click', rowInfo.id, {priority: 'event'})
     }
   }"),
       highlight = F, compact = T, style = list(color = "#000000"),
--- ui_table_module.R
+++ ui_table_module.R
@@ -1,6 +1,82 @@
 
-# Move to own functions file
+### Plotting functions and helpers --------
 
+# Colors
+s_color <- "#005387"
+c_color <- "#0096D6"
+grey_color <- "#EEEEEE"
+orange_color <- "#E88727"
+
+# Render a bar chart with a label on the left - this is the simple version used in Example Table 2
+bar_chart <- function(label, width = "100%", height = "1rem", fill = s_color, background = grey_color) {
+  bar <- div(style = list(background = fill, width = width, height = height))
+  chart <- div(style = list(flexGrow = 1, marginLeft = "0.5rem", background = background), bar)
+  div(style = list(display = "flex", alignItems = "center", color = fill), 
+      paste0(label, "%"), # text 
+      chart) # chart
+}
+
+# Render a bar chart showing the size range (as a function of the species' Lmax) that can be harvested 
+size_range_chart <- function(label, height = "0.5rem", fill = orange_color, background = grey_color, slot_limit = "N", min_size = 50, max_size = 100, Lmin = 0, Lmax = 100) {
+  
+  if(slot_limit == "N"){
+    
+    percent_out <- ((min_size)/(Lmax-Lmin))*100
+    out_bar <- div(style = list(background = background, width = paste0(round(percent_out, 2), "%"), height = height))
+    bar <- div(style = list(flexGrow = 1, marginLeft = "0.5rem", background = fill), out_bar)
+    tagList(tags$p(label),
+            bar)
+    
+  }else if(slot_limit == "Y"){
+    
+    percent_low <- ((min_size)/(Lmax-Lmin))*100
+    percent_high <- ((Lmax-max_size)/(Lmax-Lmin))*100
+    percent_in <- 100-(percent_low+percent_high)
+    out_bar_low <- div(style = list(background = background, width = paste0(round(percent_low, 2), "%"), height = height, float = "left"))
+    in_bar <- div(style = list(background = fill, width = paste0(round(percent_in, 2), "%"), height = height, float = "left"))
+    out_bar_high <- div(style = list(background = background, width = paste0(round(percent_high, 2), "%"), height = height, float = "left"))
+    bar <- div(style = list(flexGrow = 1, marginLeft = "0.5rem", background = fill), out_bar_low, in_bar, out_bar_high)
+    tagList(tags$p(label),
+            bar)
+    
+  }
+}
+
+# Donut chart with a label in the middle
+donut_chart <- function(value, fill = s_color, background = grey_color) {
+  
+  d <- tibble(category = c("value", "extra"),
+              values = c(value, 100 - value)) %>%
+    mutate(ymax = cumsum(values),
+           ymin = c(0, head(ymax, n=-1)))
+  
+  p <- ggplot(d)+
+    aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=category) +
+    geom_rect() +
+    coord_polar(theta="y") + # Try to remove that to understand how the chart is built initially
+    donut_theme +
+    scale_fill_manual(values = c(background, fill))+
+    xlim(.5, 4) +
+    annotate(geom = 'text', x = 0.5, y = 0, label = paste0(value, "%"), color = fill)
+  return(p)
+}
+
+# Theme for donut chart
+donut_theme <- theme_void()+
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_rect(color = NA_character_, fill = "transparent"),
+    plot.background = element_rect(color = NA_character_, fill = "transparent"),
+    panel.grid=element_blank(),
+    axis.ticks = element_blank(),
+    plot.title=element_blank(),
+    axis.text = element_blank(),
+    legend.position = "none"
+  )
+
+# Stars filled based on the rating
 rating_stars <- function(rating, max_rating = 5) {
   star_icon <- function(empty = FALSE) {
     tagAppendAttributes(shiny::icon("star"),
@@ -16,15 +92,28 @@ rating_stars <- function(rating, max_rating = 5) {
   div(title = label, role = "img", stars)
 }
 
-grey_color <- "#EEEEEE"
-orange_color <- "#E88727"
+# Little colored circular badges
+status_badge <- function(value, color = "#aaa", background = "#fff", border_color = "#aaa", width = "80px", height = "25px") {
+  tags$div(style = paste0("display: inline-block; 
+                          padding: 0.125rem 0.75rem; 
+                          font-weight: 600; 
+                          font-size: 0.75rem; 
+                          border: 2px solid ", border_color, "; 
+                          border-radius: 15px; 
+                          color:", color, "; 
+                          background:", background, "; 
+                          width:", width, "; 
+                          height: ", height, ";"),
+           paste0(value, "%"))
+}
 
+### UI --------
 
 interactiveTable <- function(){
   fluidPage(
     
     # Modal for facilitator inputs
-    bsModal("inputModal", title = "", trigger = "table_click", size = "large",
+    bsModal("input_modal", title = "", trigger = "table_click", size = "large",
             tagList(
               # Reactive title
               uiOutput("selected_row"),
@@ -92,6 +181,15 @@ interactiveTable <- function(){
                        )
                 )
               )
+            ),
+            # Add custom footer with different close button - This is perhaps uncessary, but I couldn't find another way to register that the user had closed the modal, which allows me to reset the selected row input. Without this, you can't click on the same row twice to edit it without first clicking on another row
+            tags$head(tags$style("#input_modal .modal-footer{ display:none}")), # hide standard footer
+            
+            tags$hr(),
+            
+            # Now add our own button with an id that we can call on the server side
+            column(2, offset = 10, align = "right",
+                   actionBttn("close_modal", "Done")
             )
     ),
     
